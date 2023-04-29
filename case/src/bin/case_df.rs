8e307698206b2242ab38caf830f6ab43440c4d17
gfzum <w48905833@163.com>
2023-04-29 15:13:55 +0800

    add double free case

--- case/src/bin/case_df.rs
+++ case/src/bin/case_df.rs
@@ -1,7 +1,7 @@
 // 问题：mirai 看不出来 double free 的 bug
 
-fn genvec() -> Vec<u8>{
-    let mut s = String::from("a tmp string");
+// 获得 s 的所有权，s 会在函数结束时被 drop
+fn genvec(mut s:String) -> Vec<u8>{
     let ptr = s.as_mut_ptr();
     unsafe{
         let v = Vec::from_raw_parts(ptr, s.len(), s.len());
@@ -11,6 +11,26 @@ fn genvec() -> Vec<u8>{
 }
 
 fn main(){
-    let v = genvec();
-    println!("{:?}", v);
+    let target = String::from("rust fuzz test");
+    println!("target string is {:?}, input your operations: ", target);
+
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input = input.trim().parse().unwrap();
+
+    match input.as_str(){
+        "length" => {
+            println!("{:?}", target.len());
+        },
+        "reverse" => {
+            println!("{:?}", target.chars().rev().collect::<String>());
+        },
+        "bytes" => {
+            let v = genvec(target);
+            println!("{:?}", v);
+        }
+        _ => {
+            println!("unknown operation {:?}", input);
+        }
+    }
 }
--- case/src/bin/case_use_free.rs
+++ case/src/bin/case_use_free.rs
@@ -1,41 +1,40 @@
-pub struct Foo{
-    data: Vec<u8>,
-}
-impl Foo{
-    pub fn new(data: &[u8]) -> Foo{
-        Foo{ data: data.to_vec() }
-    }
-    pub fn as_ptr(&self) -> &Foo{
-        // return a raw pointer of this struct
-        self
-    }
-    pub fn test(&self){
-        println!("test");
-    }
+// pub struct Foo{
+//     data: Vec<u8>,
+// }
+// impl Foo{
+//     pub fn new(data: &[u8]) -> Foo{
+//         Foo{ data: data.to_vec() }
+//     }
+//     pub fn as_ptr(&self) -> &Foo{
+//         // return a raw pointer of this struct
+//         self
+//     }
+//     pub fn test(&self){
+//         println!("test");
+//     }
     
-}
+// }
 
-pub unsafe fn use_raw_ptr(ptr: *const Foo){
-    println!("{:?}", (*ptr).test());
-}
+// pub unsafe fn use_raw_ptr(ptr: *const Foo){
+//     println!("{:?}", (*ptr).test());
+// }
 
 fn main(){
-    let data: Option<&[u8]> = Some(b"a");
-    let p = match data{
-        Some(data) => {
-            let foo = Foo::new(data);
-            // foo.as_ptr()
-            // &foo as *const Foo
-            // &foo
-        } 
-        // None => std::ptr::null_mut(),
-        None => {
-            let foo = Foo::new(b"b");
-            // &foo
-        },
-    };
-    // unsafe {(*p).test()};
-    // println!("{:?}", foo);
-    // unsafe{ use_raw_ptr(p) };
-    
+    // let data: Option<&[u8]> = Some(b"a");
+    // let p = match data{
+    //     Some(data) => {
+    //         let foo = Foo::new(data);
+    //         // foo.as_ptr()
+    //         // &foo as *const Foo
+    //         // &foo
+    //     } 
+    //     // None => std::ptr::null_mut(),
+    //     None => {
+    //         let foo = Foo::new(b"b");
+    //         // &foo
+    //     },
+    // };
+    // // unsafe {(*p).test()};
+    // // println!("{:?}", foo);
+    // // unsafe{ use_raw_ptr(p) };
 }
